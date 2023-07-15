e0bb413f2f3fe1aae79280dc89b6485343b0b4f9
Kay <levsucks584@outlook.com>
2023-07-14 23:11:30 -0400

    Merged examples

---
+++ examples/cursor.rs
@@ -0,0 +1,48 @@
+use std::{
+    io::{stdin, stdout, Write},
+    time::Duration,
+};
+
+use termip::utils::{
+    self, enable_raw_mode, erase_entire_screen, hide_cursor, move_cursor, show_cursor, disable_raw_mode, enter_alternate_buffer, leave_alternate_buffer,
+};
+
+fn main() -> std::io::Result<()> {
+    let mut inp = stdin();
+    let mut out = stdout();
+
+    enter_alternate_buffer(&mut out)?;
+    enable_raw_mode(&mut inp)?;
+
+    let (x, y) = utils::get_size(&out)?;
+
+    let x = x / 2;
+    let y = y / 2;
+
+    move_cursor(&mut out, x, y)?;
+
+    hide_cursor(&mut out)?;
+
+    out.flush()?;
+
+    std::thread::sleep(Duration::from_secs(1));
+
+    show_cursor(&mut out)?;
+
+    out.flush()?;
+
+    std::thread::sleep(Duration::from_secs(1));
+
+    move_cursor(&mut out, 10, 10)?;
+    
+    out.flush()?;
+
+    std::thread::sleep(Duration::from_secs(1));
+
+    show_cursor(&mut out)?;
+    move_cursor(&mut out, 0, 0)?;
+    disable_raw_mode(&mut inp)?;
+    leave_alternate_buffer(&mut out)?;
+
+    Ok(())
+}
